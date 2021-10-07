e6aedbcf9890215ec55cb5bd91d99005f0e989e6
hakuseo <hakuseo@gmail.com>
2021-10-08 00:40:24 +0900

    새로고침시 1초 깎이는 현상, 타이머 0초 리셋 될 때 초기화 안되는 부분 수정, CSS 모바일 화면 사이즈 조정

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const startBtn = document.querySelector('.start');
-const pauseBtn = document.querySelector('.pause');
+const modeCheck = document.querySelector('.mode-check');
 const stopBtn = document.querySelector('.stop');
 const meatballs = document.querySelector('.meatballs');
 const navBar = document.querySelector('.nav-bar');
-const modeCheck = document.querySelector('.mode-check');
-const audio = document.querySelector('.audio');
 const countdownEl = document.querySelector('.countdown');
+const startBtn = document.querySelector('.start');
+const pauseBtn = document.querySelector('.pause');
+const audio = document.querySelector('.audio');
 const fifteen = document.getElementById(15);
 const thirty = document.getElementById(30);
 const fortyFive = document.getElementById(45);
@@ -20,16 +20,16 @@ function saveTime() {
   localStorage.setItem(startTime, JSON.stringify(startTimeSet));
 }
 
-meatballs.addEventListener('click', () => {
-  navBar.classList.toggle('nav-bar-block');
-});
-
 modeCheck.addEventListener('click', (e) => {
   if (e.target.checked) {
     document.documentElement.setAttribute('color-theme', 'dark');
   } else {
     document.documentElement.setAttribute('color-theme', 'light');
   }
+});
+
+meatballs.addEventListener('click', () => {
+  navBar.classList.toggle('nav-bar-block');
 });
 
 function startTimeEvent(time) {
@@ -45,7 +45,6 @@ function startTimeEvent(time) {
     countdownEl.innerText = `${time}:00`;
     navBar.classList.remove('nav-bar-block');
     saveTime();
-    startBtn.addEventListener('click', startTimer);
   }
 }
 
@@ -54,121 +53,70 @@ function navBarClickEvent(event) {
   startTimeEvent(timer);
 }
 
+startBtn.addEventListener('click', startTimer);
+
 function startTimer(currentTime) {
   let timeMin = startTimeSet[0].time;
-  let timeSec = startTimeSet[0].timeSec;
   currentTime = currentTimeSet[0];
-  if (
-    currentTime === undefined ||
-    currentTime == 900 ||
-    currentTime == 1800 ||
-    currentTime == 2700
-  ) {
-    let countDown = setInterval(function () {
-      let min = Math.floor(timeSec / 60);
-      let sec = timeSec % 60;
-      min = min < 10 ? '0' + min : min;
-      sec = sec < 10 ? '0' + sec : sec;
-      countdownEl.innerHTML = `${min}:${sec}`;
-      timeSec--;
-      currentTimeSet.splice(0, 1, timeSec);
-      saveTime();
-
-      if (min == 0 && sec == 0) {
-        timeSec;
-        audio.play();
-        audio.volume = 0.2;
-        clearInterval(countDown);
-        startBtn.style.visibility = 'visible';
-        pauseBtn.style.visibility = 'hidden';
-      }
-    }, 1000);
-
-    startBtn.style.visibility = 'hidden';
-    pauseBtn.style.visibility = 'visible';
-    fifteen.disabled = true;
-    thirty.disabled = true;
-    fortyFive.disabled = true;
-
-    pauseBtn.addEventListener('click', () => {
+  let countDown = setInterval(function () {
+    let min = Math.floor(currentTime / 60);
+    let sec = currentTime % 60;
+    min = min < 10 ? '0' + min : min;
+    sec = sec < 10 ? '0' + sec : sec;
+    countdownEl.innerHTML = `${min}:${sec}`;
+    currentTimeSet.splice(0, 1, currentTime);
+    currentTime--;
+    saveTime();
+    if (min == 0 && sec == 0) {
       clearInterval(countDown);
-      startBtn.style.visibility = 'visible';
-      pauseBtn.style.visibility = 'hidden';
-      fifteen.disabled = false;
-      thirty.disabled = false;
-      fortyFive.disabled = false;
-    });
-
-    stopBtn.addEventListener(
-      'click',
-      () => {
-        clearInterval(countDown);
-        timeMin = timeMin;
+      audio.play();
+      audio.volume = 0.2;
+      setTimeout(() => {
         currentTime = startTimeSet[0].timeSec;
+        let min = Math.floor(currentTime / 60);
+        let sec = currentTime % 60;
+        min = min < 10 ? '0' + min : min;
+        sec = sec < 10 ? '0' + sec : sec;
+        countdownEl.innerHTML = `${min}:${sec}`;
         currentTimeSet.splice(0, 1, currentTime);
         saveTime();
-        countdownEl.innerText = `${timeMin}:00`;
         startBtn.style.visibility = 'visible';
         pauseBtn.style.visibility = 'hidden';
-        fifteen.disabled = false;
-        thirty.disabled = false;
-        fortyFive.disabled = false;
-      },
-      1000,
-    );
-  } else {
-    let countDown = setInterval(function () {
-      let min = Math.floor(currentTime / 60);
-      let sec = currentTime % 60;
-      min = min < 10 ? '0' + min : min;
-      sec = sec < 10 ? '0' + sec : sec;
-      countdownEl.innerHTML = `${min}:${sec}`;
-      currentTime--;
+      }, 1800);
+    }
+  }, 1000);
+  startBtn.style.visibility = 'hidden';
+  pauseBtn.style.visibility = 'visible';
+  fifteen.disabled = true;
+  thirty.disabled = true;
+  fortyFive.disabled = true;
+
+  pauseBtn.addEventListener('click', () => {
+    clearInterval(countDown);
+    startBtn.style.visibility = 'visible';
+    pauseBtn.style.visibility = 'hidden';
+    fifteen.disabled = false;
+    thirty.disabled = false;
+    fortyFive.disabled = false;
+  });
+
+  stopBtn.addEventListener(
+    'click',
+    () => {
+      clearInterval(countDown);
+      timeMin = timeMin;
+      currentTime = currentTimeSet[0];
       currentTimeSet.splice(0, 1, currentTime);
       saveTime();
-
-      if (min == 0 && sec == 0) {
-        currentTime;
-        audio.play();
-        audio.volume = 0.2;
-        clearInterval(countDown);
-        startBtn.style.visibility = 'visible';
-        pauseBtn.style.visibility = 'hidden';
-      }
-    }, 1000);
-    startBtn.style.visibility = 'hidden';
-    pauseBtn.style.visibility = 'visible';
-    fifteen.disabled = true;
-    thirty.disabled = true;
-    fortyFive.disabled = true;
-
-    pauseBtn.addEventListener('click', () => {
-      clearInterval(countDown);
+      countdownEl.innerText = `${timeMin}:00`;
       startBtn.style.visibility = 'visible';
       pauseBtn.style.visibility = 'hidden';
       fifteen.disabled = false;
       thirty.disabled = false;
       fortyFive.disabled = false;
-    });
-
-    stopBtn.addEventListener(
-      'click',
-      () => {
-        clearInterval(countDown);
-        timeMin = timeMin;
-        currentTime = startTimeSet[0].timeSec;
-        currentTimeSet.splice(0, 1, currentTime);
-        saveTime();
-        countdownEl.innerText = `${timeMin}:00`;
-        startBtn.style.visibility = 'visible';
-        pauseBtn.style.visibility = 'hidden';
-        fifteen.disabled = false;
-        thirty.disabled = false;
-        fortyFive.disabled = false;
-      },
-      1000,
-    );
-  }
+    },
+    1000,
+  );
 }
 
 function loadStartTime() {
@@ -189,13 +137,14 @@ function loadStartTime() {
       sec = sec < 10 ? '0' + sec : sec;
       countdownEl.innerHTML = `${min}:${sec}`;
       currentTimeSet.splice(0, 1, current);
-      console.log(currentTimeSet);
+      // console.log(current, min, sec);
     });
   }
 }
 
 function init() {
   loadStartTime();
+
   navBar.addEventListener('click', navBarClickEvent);
   if (startTimeSet[0] === undefined) {
     startTimeEvent(30);
@@ -203,3 +152,68 @@ function init() {
 }
 
 init();
+
+// function setIntervalTimer() {
+//   if (timeCondition.includes(currentTime)) {
+
+//   }
+// }
+
+//   let timeMin = startTimeSet[0].time;
+//   let timeSec = startTimeSet[0].timeSec;
+//   currentTime = currentTimeSet[0];
+//   const timeCondition = [undefined, 900, 1800, 2700];
+//   if (timeCondition.includes(currentTime)) {
+//     let countDown = setInterval(function () {
+//       let min = Math.floor(timeSec / 60);
+//       let sec = timeSec % 60;
+//       min = min < 10 ? '0' + min : min;
+//       sec = sec < 10 ? '0' + sec : sec;
+//       countdownEl.innerHTML = `${min}:${sec}`;
+//       timeSec--;
+//       currentTimeSet.splice(0, 1, timeSec);
+//       saveTime();
+
+//       if (min == 0 && sec == 0) {
+//         timeSec;
+//         audio.play();
+//         audio.volume = 0.2;
+//         clearInterval(countDown);
+//         startBtn.style.visibility = 'visible';
+//         pauseBtn.style.visibility = 'hidden';
+//       }
+//     }, 1000);
+
+//     startBtn.style.visibility = 'hidden';
+//     pauseBtn.style.visibility = 'visible';
+//     fifteen.disabled = true;
+//     thirty.disabled = true;
+//     fortyFive.disabled = true;
+
+//     pauseBtn.addEventListener('click', () => {
+//       clearInterval(countDown);
+//       startBtn.style.visibility = 'visible';
+//       pauseBtn.style.visibility = 'hidden';
+//       fifteen.disabled = false;
+//       thirty.disabled = false;
+//       fortyFive.disabled = false;
+//     });
+
+//     stopBtn.addEventListener(
+//       'click',
+//       () => {
+//         clearInterval(countDown);
+//         timeMin = timeMin;
+//         currentTime = startTimeSet[0].timeSec;
+//         currentTimeSet.splice(0, 1, currentTime);
+//         saveTime();
+//         countdownEl.innerText = `${timeMin}:00`;
+//         startBtn.style.visibility = 'visible';
+//         pauseBtn.style.visibility = 'hidden';
+//         fifteen.disabled = false;
+//         thirty.disabled = false;
+//         fortyFive.disabled = false;
+//       },
+//       1000,
+//     );
+//   }
